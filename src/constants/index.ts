d1c3e0d5f448298445b508e821775cdba76f7128
Kartavya Bang <bangkartavya@gmail.com>
2025-03-04 14:14:00 +0530

    Added : Workshops conducted last year

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -25,5 +25,25 @@ export const upcomingEvents: EventType[] = [
 ];
 
 export const activities: ActivityType[] = [
-
+    {
+        id: 1,
+        title: "Getting started with Git and Docker",
+        description: "A introductory workshop on basics of Git and Docker with a little of C programming",
+        date: "05/09/2024",
+        participants: 60
+    },
+    {
+        id: 2,
+        title: "Introduction to CAD",
+        description: "A introductory workshop on basics of CAD",
+        date: "22/10/2024",
+        participants: 55
+    },
+    {
+        id: 3,
+        title: "Python Programming for Machine Learning",
+        description: "Introduction to Machine Learning using Python",
+        date: "28/12/2024",
+        participants: 60
+    },
 ];
