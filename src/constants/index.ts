42204b7f919b9fbca0da5faa554ccd0d725b09da
Kartavya Bang <bangkartavya@gmail.com>
2025-03-08 13:55:27 +0530

    Removed : Activities from constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-import { ActivityType, EventType } from "@/types";
+import { EventType } from "@/types";
 
 export const upcomingEvents: EventType[] = [
     {
@@ -10,29 +10,4 @@ export const upcomingEvents: EventType[] = [
         location: "L26-27, New Academic Block",
         capacity: 100,
     }
-];
-
-
-export const activities: ActivityType[] = [
-    {
-        id: 1,
-        title: "Getting started with Git and Docker",
-        description: "A introductory workshop on basics of Git and Docker with a little of C programming",
-        date: "05/09/2024",
-        participants: 60
-    },
-    {
-        id: 2,
-        title: "Introduction to CAD",
-        description: "A introductory workshop on basics of CAD",
-        date: "22/10/2024",
-        participants: 55
-    },
-    {
-        id: 3,
-        title: "Python Programming for Machine Learning",
-        description: "Introduction to Machine Learning using Python",
-        date: "28/12/2024",
-        participants: 60
-    },
 ];
