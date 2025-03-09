7c49a65bd0badea7f30eab67d4ec479972ae50cb
Kartavya Bang <bangkartavya@gmail.com>
2025-03-09 22:27:22 +0530

    Updated : Pipeline to include more variables , admin page to check for unauthorized users

--- src/pages/admin/page.tsx
+++ src/pages/admin/page.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import { Button } from '@/components/ui/button';
+import { client } from '@/lib/supabase/supabase';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react'
 
 const AdminPage = () => {
+    const router = useRouter();
+    const [validUser, setValidUser] = useState(false);
+    useEffect(() => {
+        const checkSession = async () => {
+            const { data: { session } } = await client.auth.getSession();
+
+            console.log(session);
+            console.log(new Date().getTime());
+
+            if (session) {
+                setValidUser(true);
+                return;
+            }
+            return;
+        }
+
+        checkSession();
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.clear();
+        router.push("/");
+    }
+
     return (
-        <div>AdminPage</div>
+        <>
+            {
+                validUser ? (
+                    <div>
+                        AdminPage
+                        <Button onClick={handleLogout}>
+                            Logout
+                        </Button>
+                    </div>
+                ) : (
+                    <div>
+                        <p>
+                            Oops!! You're not supposed to be here!!
+                        </p>
+                        <Button onClick={handleLogout}>
+                            Return To Home
+                        </Button>
+                    </div>
+
+                )
+            }
+        </>
     )
 }
 
