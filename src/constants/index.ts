52b6cd6f74302a505ba1f7f7093c52f1501cd9eb
Kartavya Bang <bangkartavya@gmail.com>
2025-03-11 11:02:20 +0530

    Fixed: type conflict

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,7 @@ import { EventType } from "@/types";
 
 export const upcomingEvents: EventType[] = [
     {
-        id: 1,
+        id: "1",
         title: "Building from Scratch",
         description: "A hands-on event to help participants take the first step toward creating something amazing. Whether you have an idea or just want to build, this event will guide you in getting started.",
         date: "2025-03-06",
