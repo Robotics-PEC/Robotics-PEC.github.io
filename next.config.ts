83cbc6438f04309db7a106bc5659c26cbcc9ff97
BangKartavya <bangkartavya@gmail.com>
2026-08-17 10:34:12 +0530

    remove : static constraints

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,24 @@
 import { NextConfig } from "next";
 
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_API_ENDPOINT;
+
+if (!supabaseUrl) {
+    throw new Error("NEXT_PUBLIC_SUPABASE_API_ENDPOINT is not configured");
+}
+
+const supabaseHostname = new URL(supabaseUrl).hostname;
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-    output: "export", // Enables static export
-    images: {
-        unoptimized: true, // Disables Next.js image optimization for static exports
-    },
     reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: supabaseHostname,
+                pathname: "/storage/v1/object/public/**",
+            },
+        ],
+    },
     eslint: {
         ignoreDuringBuilds: true,
     },
