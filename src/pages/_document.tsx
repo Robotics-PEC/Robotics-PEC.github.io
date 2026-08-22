ebec0bb3b655063f7977e64acec970812ded207b
Kartavya Bang <bangkartavya@gmail.com>
2026-08-22 11:35:48 +0530

    add : google analyitics (#59)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function Document() {
+  const gaID = process.env.NEXT_PUBLIC_GA_ID ?? "";
   return (
     <Html lang="en">
       <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
+        <GoogleAnalytics gaId={gaID} />
       </body>
     </Html>
   );
