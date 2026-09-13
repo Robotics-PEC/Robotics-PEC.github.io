17308324f5b63b46d7d131363eb48880973239e4
BangKartavya <bangkartavya@gmail.com>
2026-09-13 15:20:47 +0530

    fix : fix the fallback again

--- next.config.ts
+++ next.config.ts
@@ -57,7 +57,7 @@ const nextConfig: NextConfig = {
             {
                 // Ensure it ignores internal Next.js assets, otherwise everything breaks
                 source: "/:path((?!_next|api|favicon.ico).*)",
-                destination: "https://github.io",
+                destination: "https://robotics-pec.github.io/:path((?!_next|api|favicon.ico).*)",
             },
         ],
         }
